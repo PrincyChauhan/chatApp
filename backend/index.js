7e32130cdad1b8933e94f025f8122b970c7235b9
PrincyChauhan <chauhanprincee7@gmail.com>
2025-03-26 15:41:04 +0530

    Signup, login and logout

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { ConnectDB } from "./config/db.js";
+import userRoute from "./route/user.route.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 ConnectDB();
+app.use("/user", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

d13485193d4ba53620abb90412e9be02fe9e47c2
PrincyChauhan <chauhanprincee7@gmail.com>
2025-03-27 17:33:29 +0530

    Logout API

--- frontend/src/home/left1/Logout.jsx
+++ frontend/src/home/left1/Logout.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { TbLogout2 } from "react-icons/tb";
+import Cookies from "js-cookie";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
 const Logout = () => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const [, setAuthUser] = useAuth();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post("/api/user/logout");
+      localStorage.removeItem("ChatApp");
+      Cookies.remove("jwt");
+      setAuthUser(undefined);
+
+      if (response.data) {
+        toast.success("Logout successful");
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error("Logout failed. Please try again.");
+    }
+  };
+
   return (
-    <div className="w-[4%] bg-slate-950 text-white flex flex-col justify-end">
-      <div className="p-3 algin-bottom">
-        <div className="flex space x-e">
-          <button>
+    <>
+      <ToastContainer />
+      <div className="w-[4%] bg-slate-950 text-white flex flex-col justify-end">
+        <div className="p-3">
+          <button onClick={handleLogout} disabled={loading}>
             <TbLogout2 className="text-5xl p-2 hover:bg-gray-600 rounded-lg duration-300" />
+            {loading && <p>Logging out...</p>}
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
